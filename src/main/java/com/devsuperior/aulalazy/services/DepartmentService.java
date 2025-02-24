c4db0e47e9bd27fa4ea76da9600fc687d0027d5d
Nelio Alves <acenelio@gmail.com>
2025-02-24 18:23:01 -0300

    Update sb 3.4.3

--- src/main/java/com/devsuperior/aulalazy/services/DepartmentService.java
+++ src/main/java/com/devsuperior/aulalazy/services/DepartmentService.java
@@ -2,9 +2,7 @@ package com.devsuperior.aulalazy.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import com.devsuperior.aulalazy.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.aulalazy.dto.DepartmentDTO;
 import com.devsuperior.aulalazy.dto.EmployeeMinDTO;
 import com.devsuperior.aulalazy.entities.Department;
+import com.devsuperior.aulalazy.entities.Employee;
 import com.devsuperior.aulalazy.repositories.DepartmentRepository;
 
 @Service
